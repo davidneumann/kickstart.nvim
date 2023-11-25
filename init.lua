61bb5141a1c1509e63340ea41a97b77834acb1e2
David Neumann <davidjneumann@gmail.com>
2023-11-24 20:52:52 -0500

    Add matchit for better matching with % support

--- init.lua
+++ init.lua
@@ -79,6 +79,8 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  'https://github.com/adelarsq/vim-matchit',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
