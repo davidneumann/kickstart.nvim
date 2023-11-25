5c6246a5912fe39e8ff80f03ec71b68ad97829ef
David Neumann <davidjneumann@gmail.com>
2023-11-24 20:57:12 -0500

    Add so many hotkeys for buffers and other items

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -63,3 +63,26 @@ end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+
+vim.keymap.set('n', "]b", "<cmd>BufferLineCycleNext<CR>", { desc = "Go to next buffer", silent = true })
+vim.keymap.set('n', "[b", "<cmd>BufferLineCyclePrev<CR>", { desc = "Go to prev buffer", silent = true })
+vim.keymap.set('n', "]B", "<cmd>BufferLineMoveNext<CR>", { desc = "Move buffer right", silent = true })
+vim.keymap.set('n', "[B", "<cmd>BufferLineMovePrev<CR>", { desc = "Move buffer left", silent = true })
+
+vim.keymap.set('n', '<leader>q', '<cmd>qa<cr>', { desc = "Close session" })
+vim.keymap.set('n', '<leader>ur', '<cmd>SessionRestore<cr>', { desc = "[R]estore session" })
+vim.keymap.set('n', '<leader>uq', '<cmd>SessionSave<cr><cmd>qa<cr>', { desc = "[Q]uit session" })
+
+vim.keymap.set('n', '<leader>ha', function() require("harpoon.mark").add_file() end, { desc = '[H]arpoon [A]dd File' })
+vim.keymap.set('n', '<leader>hr', function() require("harpoon.mark").remove_file() end,
+  { desc = '[H]arpoon [R]emove File' })
+vim.keymap.set('n', '<leader>hs', '<cmd>Telescope harpoon marks<cr>', { desc = '[H]arpoon [S]search' })
+vim.keymap.set('n', '<leader>sh', '<cmd>Telescope harpoon marks<cr>', { desc = '[S]search [H]arpoon' })
+vim.keymap.set('n', '<leader>h1', function() require("harpoon.ui").nav_file(1) end, { desc = '[H]arpoon Goto File [1]' })
+vim.keymap.set('n', '<leader>h2', function() require("harpoon.ui").nav_file(2) end, { desc = '[H]arpoon Goto File [2]' })
+vim.keymap.set('n', '<leader>h3', function() require("harpoon.ui").nav_file(3) end, { desc = '[H]arpoon Goto File [3]' })
+vim.keymap.set('n', '<leader>h4', function() require("harpoon.ui").nav_file(4) end, { desc = '[H]arpoon Goto File [4]' })
+
+vim.keymap.set('n', '<leader>bd', ':bd<cr>', { desc = "[B]uffer [D]elete / Close" })
+vim.keymap.set('n', '<leader>bs', ':w<cr>', { desc = "[B]uffer [S]ave" })
