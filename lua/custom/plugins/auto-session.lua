5645dacdc30d8ccde5f1a71710762b08d845b55e
David Neumann <davidjneumann@gmail.com>
2023-11-24 20:55:55 -0500

    Add auto session saving and restoration

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,21 @@
+function _G.close_all_floating_wins()
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local config = vim.api.nvim_win_get_config(win)
+		if config.relative ~= '' then
+			vim.api.nvim_win_close(win, false)
+		end
+	end
+
+	local api = require("nvim-tree.api")
+	api.tree.close()
+end
+
+return {
+	'rmagatti/auto-session',
+	config = function()
+		require('auto-session').setup {
+			log_level = "error",
+			pre_save_cmds = { _G.close_all_floating_wins },
+		}
+	end
+}
