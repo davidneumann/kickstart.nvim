96e13f0a91271b23d59fd55c34d5c0ec1b11dffd
David Neumann <davidjneumann@gmail.com>
2023-12-06 15:45:09 -0500

    Add neorg

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -7,8 +7,13 @@ return {
 			load = {
 				["core.defaults"] = {}, -- Loads default behaviour
 				["core.concealer"] = {}, -- Adds pretty icons to your documents
-				-- ["core.completion"] = {},
-				["core.dirman"] = {  -- Manages Neorg workspaces
+				["core.completion"] = {
+					config = {
+						engine = "nvim-cmp"
+					}
+				},
+				["core.summary"] = {},
+				["core.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {
 							notes = "~/notes",
