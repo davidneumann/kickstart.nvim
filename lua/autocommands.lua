fe7071b07b1b619e5b726e48dcba503cec1f708c
David Neumann <davidjneumann@gmail.com>
2024-01-09 13:42:53 -0500

    Add dot support

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -4,6 +4,16 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.dot" },
+  callback = function()
+    vim.lsp.start({
+      name = "dot",
+      cmd = { "dot-language-server", "--stdio" }
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*.norg" },
   callback = function()
     vim.opt.cc = ""
