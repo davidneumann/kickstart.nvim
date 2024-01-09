f585b6bba0414caafaf0b67334eddd799bcb7c6e
David Neumann <davidjneumann@gmail.com>
2024-01-09 13:40:46 -0500

    Disable column in norg files

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -2,3 +2,10 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = '*.ejs',
   command = 'set filetype=embedded_template'
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.norg" },
+  callback = function()
+    vim.opt.cc = ""
+  end,
+})
