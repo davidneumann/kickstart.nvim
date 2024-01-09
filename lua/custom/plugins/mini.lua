868b81f0ee694a9e8e8aabe19645701c6e36351a
David Neumann <davidjneumann@gmail.com>
2024-01-09 13:44:30 -0500

    Disable mini.animate in hopes of a faster experience

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,7 +1,8 @@
-return {
-	'echasnovski/mini.nvim',
-	version = '*',
-	config = function()
-		require("mini.animate").setup()
-	end
-}
+-- return {
+-- 	'echasnovski/mini.nvim',
+-- 	version = '*',
+-- 	config = function()
+-- 		require("mini.animate").setup()
+-- 	end
+-- }
+return {}
