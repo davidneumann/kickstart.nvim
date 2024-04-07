585a97a493704230f504adaa3c0e264b01bfc73b
David Neumann <davidjneumann@gmail.com>
2024-04-07 02:43:35 -0400

    bugfix: Add missing mason deps

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -88,6 +88,8 @@ return {
 		"theHamsta/nvim-dap-virtual-text",
 		"leoluz/nvim-dap-go",
 		"folke/neodev.nvim",
+		'williamboman/mason.nvim',
+		'jay-babu/mason-nvim-dap.nvim',
 	},
 	config = function()
 		require("neodev").setup({ library = { plugins = { "nvim-dap-ui" }, types = true }, })
@@ -111,22 +113,22 @@ return {
 			clear_on_continue = true
 		})
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_setup = true,
+		require('mason-nvim-dap').setup {
+			-- Makes a best effort to setup the various debuggers with
+			-- reasonable debug configurations
+			automatic_setup = true,
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+			-- You can provide additional configuration to the handlers,
+			-- see mason-nvim-dap README for more information
+			handlers = {},
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'js-debug-adapater'
-      },
-    }
+			-- You'll need to check that you have the required things installed
+			-- online, please don't ask me how to install them :)
+			ensure_installed = {
+				-- Update this to ensure that you have the debuggers for the langs you want
+				'js-debug-adapater'
+			},
+		}
 
 		dap.adapters["pwa-node"] = {
 			type = "server",
