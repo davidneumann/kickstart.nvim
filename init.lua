8d234efb461d3733f6e657f46bda5f5036175e87
David Neumann <davidjneumann@gmail.com>
2024-05-14 17:50:58 -0400

    feat: Add dadbod DB UI

--- init.lua
+++ init.lua
@@ -645,5 +645,12 @@ cmp.setup {
   },
 }
 
+cmp.setup.filetype({ "sql" }, {
+  sources = {
+    { name = "vim-dadbod-completion" },
+    { name = "buffer" },
+  }
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
